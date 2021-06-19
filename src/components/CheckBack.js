05413db126399e61bc36e66d65926c525735a4b6
Jose Guaro <>
2021-06-19 14:40:22 -0700

    itinerary included

--- src/components/CheckBack.js
+++ src/components/CheckBack.js
@@ -9,7 +9,7 @@ class CheckBack extends React.Component{
    render(){
      return(
       <section id="checkBack" className="d-flex align-items-center justify-content-center">
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQm1XVz_3qt_lI_p7QzxLp320_FogQGQV2FZVqoVIjUnLiapLH2o8H7VwHYZ14bpjC9xtpT6lGJ4VFR/pubhtml?gid=1919304817&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQm1XVz_3qt_lI_p7QzxLp320_FogQGQV2FZVqoVIjUnLiapLH2o8H7VwHYZ14bpjC9xtpT6lGJ4VFR/pubhtml?widget=false&amp;headers=false"></iframe>
       </section>
    );
   }
