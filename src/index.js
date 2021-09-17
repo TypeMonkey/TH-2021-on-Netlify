dbfb763b9b25221392363f207f243d0c6570aeb0
Jose Guaro <>
2021-09-17 15:12:46 -0700

    new site

--- src/index.js
+++ src/index.js
@@ -7,11 +7,22 @@ import ReactDOM from 'react-dom';
 import './css/Variables.css';
 import './css/index.css';
 import Landing from './Landing';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+const shortLinks = {
+  'recruiting': 'https://ucsd.zoom.us/my/fashai'
+}
 ReactDOM.render(
   <React.StrictMode>
     {/* Add routing later for the form */}
-    <Landing/>
+    <Router>
+      <Switch>
+        { Object.keys(shortLinks).map(key => (
+          <Route exact path={`/${key}`} component={() => window.location = shortLinks[key]} />
+        ))}
+        <Route path='/' component={Landing} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
